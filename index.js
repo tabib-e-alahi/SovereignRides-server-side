135e7b99e40c1e85428f215deb34f73a78777a74
tabib-e-alahi <etabib.alahi@gmail.com>
2023-10-20 20:30:54 +0600

    vercel setup

--- index.js
+++ index.js
@@ -86,6 +86,14 @@ async function run() {
       res.send(result)
     });
 
+    app.delete("/carCart/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    });
+
+
     
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
